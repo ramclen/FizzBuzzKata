22d1bf5a9e9947f25622f0f798ef38b03cfae426
Cristopher López Santana <cristopher.correo@gmail.com>
2015-07-10 14:51:32 +0100

    create a main code with the simple html

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,21 @@
-define(function () {
-    alert("Hello World");
-});
+define(["src/FizzBuzz"], function () { return Main().main();});
+function Main(){
+
+    function main() {
+        $("#toTransform").on("keyup", function () {
+            outputValue(FizzBuzz().transform(inputValue()).toString());
+        })
+    }
+
+    function inputValue() {
+        return eval($("#toTransform").val());
+    }
+
+    function outputValue(value) {
+        return $("#transformed").val(value);
+    }
+
+    return {
+        main : main
+    }
+}
